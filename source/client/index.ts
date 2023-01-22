5f4c236c3158d06e7857bb8d4ff5e045ca1802b3
Field-Tester <interclosure@gmail.com>
2023-01-23 00:54:48 +0200

    Refactored client variable

--- source/client/index.ts
+++ source/client/index.ts
@@ -3,13 +3,13 @@ import { registerEvents } from "../utils";
 import Events from "../events";
 import ClientKeys from "../keys";
 
-const Resolver = new Client({
+const Keeb = new Client({
   intents: 32767
 });
 
-registerEvents(Resolver, Events);
+registerEvents(Keeb, Events);
 
-Resolver.login(ClientKeys.token).catch((error) => {
+Keeb.login(ClientKeys.token).catch((error) => {
   console.error(`[e] Login error! > ${error}`);
   process.exit(1);
 });
